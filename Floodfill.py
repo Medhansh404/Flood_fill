7f73285e1bd9ee481b06def6f7ab55fa9c641649
Medhansh404 <21ucs126@lnmiit.ac.in>
2023-12-26 21:34:22 +0530

    Somewhat final on basic stuff point1, 2, 3 are done

--- Floodfill.py
+++ Floodfill.py
@@ -5,9 +5,10 @@ from numpy import ndarray
 from PIL import Image
 
 # Way 1
-'''img = cv2.imread('stock_img.jpeg', 1)
+img = cv2.imread('stock_img.jpeg', 1)
 print(type(img))
 
+'''
 print(len(img[0]), len(img))
 print(img[0][0])
 # 474X474 image
@@ -17,9 +18,10 @@ cv2.destroyWindow('image')
 '''
 
 # Way 2
-filename = "stock_img.jpeg"
+'''filename = "stock_img.jpeg"
 with Image.open(filename) as img:
     height, width = img.shape
+'''
 
 
 # 1). Task done img imported and converted to workable format
@@ -32,7 +34,6 @@ def most_frequent_color(image) -> tuple[Any, Any]:
     '''
     r_img = img.reshape(-1, img.shape[-1])  # two dimensions pixels and colors
     colors, count = np.unique(r_img, axis=0, return_counts=True)
-    print(sorted(count))
     max_index = np.argmax(count)
     min_index = np.argmin(count)
     return colors, colors[max_index], colors[min_index]
@@ -42,14 +43,27 @@ _, a, b = most_frequent_color(img)
 print(a, b)
 
 
-def color_dump(image, min_color, max_color) -> List[List[int]]:
+def color_dump(image, min_color, max_color):
     # Trying bfs
     req_pixels = image[
         (image[:, :, 0] == max_color[0]) & (image[:, :, 1] == max_color[1]) & (image[:, :, 2] == max_color[2])]
-    print(req_pixels[0:6])
+
     # for i in req_pixels:
 
     return []
 
 
-test = color_dump(img, b, a)
+# Replace most used color with a new color
+def replace_color(image, old_color, new_color):
+    mask = np.all(image == old_color, axis=-1)
+    image[mask] = new_color
+
+
+# Replace most used color with green (you can replace it with the desired color)
+replace_color(img, a, b)
+
+# test = color_dump(img, b, a)
+cv2.imwrite('stock_img.jpeg', img)
+cv2.imshow('stock_img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
